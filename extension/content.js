5b24127c84d05bc22f9f5a6b4cc5e874fa5ef07b
Gabriel Nadaleti <nadaletriel@gmail.com>
2026-06-04 15:58:09 -0300

    feat: add album support and improve query string search

--- extension/content.js
+++ extension/content.js
@@ -4,17 +4,76 @@ if(urlRegex.test(location.href) && location.href.includes('track')) {
     const spotifyInterval = setInterval(() => {
         try {
             const songName = document.querySelector('[data-testid="creator-link"]').parentElement.parentElement.parentElement.children[2].innerText
-            const author = document.querySelector('[data-testid="creator-link"]').parentElement.parentElement.parentElement.children[0].children[1].innerText
+            const artists = document.querySelectorAll("[data-testid='track-artist-link-card']")
 
-            if(songName && author) {
+            let artistNames = '';
+            for (let i = 0; i < artists.length; i++) {
+                artistNames += artists[i].innerText.split('\n')[1];
+                if (i < artists.length - 1) {
+                    artistNames += ', ';
+                }
+            }
+
+            if(songName && artistNames) {
                 chrome.storage.sync.set({fromSpotify: true})
                 clearInterval(spotifyInterval);
                 
-                location.href = `https://music.youtube.com/search?q=${encodeURI(`${songName} ${author}`)}`
+                location.href = `https://music.youtube.com/search?q=${encodeURI(`${songName} ${artistNames}`)}`
             }
-        } catch (ex) {}
+        } catch (ex) {
+        }
     }, 100);
 }
+
+if(urlRegex.test(location.href) && location.href.includes('album')) {
+    const spotifyAlbumInterval = setInterval(() => {
+        try {
+            const title = document.querySelector('[data-testid="entityTitle"]').innerText;
+            const author = document.querySelector('[data-testid="creator-link"]').parentElement.parentElement.parentElement.children[0].children[1].innerText
+
+            if(title && author) {
+                chrome.storage.sync.set({fromSpotifyAlbum: true})
+                clearInterval(spotifyAlbumInterval);
+                
+                location.href = `https://music.youtube.com/search?q=${encodeURI(`${title} ${author}`)}`
+            }
+        } catch (ex) {
+        }
+    }, 100);
+}
+
+chrome.storage.sync.get('fromSpotifyAlbum').then(result => {
+    if(result.fromSpotifyAlbum) {
+        const youtubeMusicInterval = setInterval(() => {
+            try {
+                const albumFilterButtons = document.querySelectorAll("yt-formatted-string.ytmusic-chip-cloud-chip-renderer");
+
+                const albumFilterButton = new Array(...albumFilterButtons).find(a => a.innerText === "Álbuns");
+
+                if(albumFilterButton) {
+                    albumFilterButton.click();
+
+                    setTimeout(() => {
+                        const selectAlbumItenval = setInterval(() => {
+                            const album = document.querySelector(".content.style-scope.ytmusic-tabbed-search-results-renderer").children[0].children[1].children[0].children[0].children[3].children[0].children[0];
+    
+                            if(album) {
+                                album.click();
+                                clearInterval(selectAlbumItenval);
+                                chrome.storage.sync.remove('fromSpotifyAlbum')
+                            }
+                        }, 100)
+                    }, 200)
+
+                    clearInterval(youtubeMusicInterval);
+                }
+            } catch (ex) {
+            }
+        }, 100);
+        
+    }
+})
+
 
 chrome.storage.sync.get('fromSpotify').then(result => {
     if(result.fromSpotify) {
@@ -28,7 +87,8 @@ chrome.storage.sync.get('fromSpotify').then(result => {
 
                     clearInterval(youtubeMusicInterval);
                 }
-            } catch (ex) {}
+            } catch (ex) {
+            }
         }, 100);
         
     }
